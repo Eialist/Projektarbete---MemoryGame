459e0348ca856f3485ac4c62af8028083862d663
Eia <elsinoree@gmail.com>
2022-11-29 20:26:35 +0100

    Memory

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function updateDisplays(players) {
 // an array with cards as objects
 const memoryCards = [{
     name: 'bee',
-    img: './assets/bee.png',
+    img: './Assets/bee.png',
 },
 {
     name: 'butterfly',
